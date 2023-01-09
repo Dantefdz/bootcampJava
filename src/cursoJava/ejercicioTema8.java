04cb7b843c95152810d3b688701d8d16057af628
Dantefdz <88099408+Dantefdz@users.noreply.github.com>
2023-01-09 15:52:51 -0300

    ejer tema 8

---
+++ src/cursoJava/ejercicioTema8.java
@@ -0,0 +1,13 @@
+package cursoJava;
+
+public class ejercicioTema8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Persona p = new Persona(23, "dante", 22162647);
+		
+		p.setEdad(24);
+		System.out.print(p.toString());
+	}
+	
+}
